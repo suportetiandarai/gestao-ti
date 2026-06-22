f6c9aefea9c16381189701324b508d08b60f467f
suportetiandarai <suporteandaraiti@gmail.com>
2026-06-22 14:36:18 -0300

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -1,4 +1,4 @@
 window.GESTAO_TI_CONFIG = Object.freeze({
     // Use somente uma chave "Publishable key" (sb_publishable_...) ou a chave anon legada.
-    SUPABASE_PUBLIC_KEY: 'COLE_A_CHAVE_PUBLICA_DO_SUPABASE_AQUI'
+    SUPABASE_PUBLIC_KEY: 'sb_publishable_-zHPJwg6WDu099Vaeqz2rw_XnTr6_Ve'
 });
